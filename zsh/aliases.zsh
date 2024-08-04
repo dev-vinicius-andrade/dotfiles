60492afdd0dc411fbd365ac5bc7949928866c18f
Vinicius Andrade <developer.vinicius.andrade@gmail.com>
2024-08-04 09:42:10 -0300

    ajusts lslha, it was inside of the check IS_WSL

--- zsh/aliases.zsh
+++ zsh/aliases.zsh
@@ -4,11 +4,11 @@ alias ll="eza --icons --group-directories-first -l"
 alias svi="sudo -E vim $@"
 alias svim="sudo -E vim $@"
 alias snvim="sudo -E nvim"
+alias lslha="ls -lha"
 if [ -n "$IS_WSL" ] && [ "$IS_WSL" = "true" ]; then
     alias docker="docker.exe"
     alias docker-compose="docker-compose.exe"
     alias cmd="cmd.exe"
     alias explorer="explorer.exe"
     alias notepad="notepad.exe"
-    alias lslha="ls -lha"
 fi
