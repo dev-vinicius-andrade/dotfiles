4ba3218ecb5fd92df02e4b787c9ea21190f2a97f
Vinicius Andrade <developer.vinicius.andrade@gmail.com>
2024-10-24 04:32:28 -0300

    tests alias

--- zsh/aliases.zsh
+++ zsh/aliases.zsh
@@ -5,6 +5,8 @@ alias svi="sudo -E vi $@"
 alias svim="sudo -E vim $@"
 alias snvim="sudo -E nvim $@"
 alias lslha="ls -lha"
+alias gopull='function _gopull() { local current_dir=$(pwd); cd "$1" && git pull; cd "$current_dir"; }; _gopull'
+
 if [ -n "$IS_WSL" ] && [ "$IS_WSL" = "true" ]; then
     alias docker="docker.exe"
     alias docker-compose="docker-compose.exe"
