43c1c0faf6e8acc4f9714a509b31ee548bc40dad
Vinicius Andrade <developer.vinicius.andrade@gmail.com>
2025-06-04 00:28:57 -0300

    better aliases

--- zsh/aliases.zsh
+++ zsh/aliases.zsh
@@ -8,17 +8,20 @@ alias lslha="ls -lha"
 alias gopull='function _gopull() { local current_dir=$(pwd); cd "$1" && git pull; cd "$current_dir"; }; _gopull'
 
 if [ -n "$IS_WSL" ] && [ "$IS_WSL" = "true" ]; then
-    alias docker="docker.exe"
-    alias docker-compose="docker-compose.exe"
+    if [ "$DISABLE_DOCKER_ALIASES" != "true" ]; then
+        alias docker="docker.exe"
+        alias docker-compose="docker-compose.exe"
+    fi
     alias cmd="cmd.exe"
     alias explorer="explorer.exe"
     alias notepad="notepad.exe"
 fi
 if command -v nix-env >/dev/null 2>&1; then
 
-  alias nxi='nix-env -i'  # install
-  alias nxu='nix-env -u'  # update
-  alias nxr='nix-env -e'  # remove
-  alias nxls='nix-env -q' # list
-  alias nxgc='nix-collect-garbage -d'
+    alias nxi='nix-env -i'  # install
+    alias nxu='nix-env -u'  # update
+    alias nxr='nix-env -e'  # remove
+    alias nxls='nix-env -q' # list
+    alias nxgc='nix-collect-garbage -d'
+    alias hmswt='home-manager switch'
 fi
